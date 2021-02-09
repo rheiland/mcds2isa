8bf5ca829d99a9d7ce0d94c06de635364e262303
Connor Burns <cjburns1197@gmail.com>
2021-02-08 19:04:29 -0500

    Version 0.2 - Used Pandas to simplify code structure, added loop to find attributes of XML children

--- Burns_Thesis_DatabaseConversion/mcds_entity_list.py
+++ Burns_Thesis_DatabaseConversion/mcds_entity_list.py
@@ -1,6 +1,6 @@
-#mcds_entity_list.py - look through all DCL's in MCDS folder, generate xlsx file with all tags/xpaths
-#   Used to determined semi-complete list of possible DCL unique tags.
-#   User should remove duplicate rows from xslx file after generating
+# mcds_entity_list.py - look through all DCL's in MCDS folder, generate xlsx file with all tags/xPath's
+# Used to determined semi-complete list of possible DCL unique tags.
+# User should remove duplicate rows from xlsx file after generating
 
 # Input:
 #   MCDS-DCL xml files in MCDS_2_ISATab\All_Digital_Cell_Lines
@@ -10,50 +10,39 @@
 
 # Author: Connor Burns
 # Date:
-#   v0.1 - Feb 2020
+#   v0.1 - Feb 2020: Initial working script
+#   v0.2 - Feb 2020: Made writing to excel file more compact, added writing child.keys
 
 import os
-import xlsxwriter
 from lxml import etree
+import pandas as pd
+import re
 
 os.chdir(r'C:\Users\Connor\PycharmProjects\MCDS_2_ISATab\Burns_Thesis_DatabaseConversion')
 cwd = os.getcwd()
-#TODO - Only works if cwd is set to directry in line 19 - update later for other users?
+# TODO - Only works if cwd is set to directory in line 21 - update later for other users?
 DCL_xml_dir = os.path.join(cwd[:-32], 'All_Digital_Cell_Lines')
-#DCL_xml_dir = (r'...<enter local path>...\MCDS_2_ISATab\All_Digital_Cell_Lines')
-
-workbook = xlsxwriter.Workbook('MCDS_DCL_All_Tags.xlsx')
-worksheet = workbook.add_worksheet()
-worksheet.write('A1','MCDS Entities', workbook.add_format({'bold': True}))
-worksheet.write('B1','xPath', workbook.add_format({'bold': True}))
-worksheet.write('C1','File Name', workbook.add_format({'bold': True}))
-#Initialize xlsx file, write column headers to workbook
-
-i = 1;
-#Row counter, used in function below
-
-def extract_tags_xPath(XML_FileName):
-    xml_test_file = os.path.join(DCL_xml_dir, XML_FileName)
-    tree = etree.parse(xml_test_file)
-    root = tree.getroot()
-
-    global i
-    for child in root.iter():
-        worksheet.write(i, 0, str(child.tag))
-        #finds all DCL tags in XML input file
-        parents = []
-        for parent in child.iterancestors():
-            parents.insert(0,parent.tag)
-        xPath = "/".join(parents)
-        #finds all parents of the tag found above, appends to list in hierarchical order, joins with / to create xPath
-        worksheet.write(i, 1, str(xPath))
-        worksheet.write(i, 2, str(XML_FileName))
-        i += 1
-
+# DCL_xml_dir = (r'...<enter local path>...\MCDS_2_ISATab\All_Digital_Cell_Lines')
 DCL_list = os.listdir(DCL_xml_dir)
-#Create list of XML files from All Cell Lines folder
 
-for DCL in DCL_list:
-    extract_tags_xPath(DCL)
+column_names = ["MCDS Entity", "xPath", "File Name", "Entity Type"]
+# Define column names
+df = pd.DataFrame(columns=column_names)
+# Create empty Pandas dataframe
+parser = etree.XMLParser(remove_comments=True)
+# Parse XML tree and remove elements that are comments
 
-workbook.close()
+for XML_FileName in DCL_list:
+    # Loop through all XML files in All_Digital_Cell_Lines folder
+    tree = etree.parse(os.path.join(DCL_xml_dir, XML_FileName), parser=parser)
+    root = tree.getroot()
+    for child in root.iter():
+        df = df.append(pd.Series([child.tag, re.sub(r"\[[0-9]]", "", tree.getpath(child)), XML_FileName, "Element"],
+                                 index=df.columns), ignore_index=True)
+        # Append all MCDS tags from file, xPath with indexing removed, and XML file to list
+        for attr in child.keys():
+            df = df.append(pd.Series([attr, ("/".join([re.sub(r"\[[0-9]]", "", tree.getpath(child)), attr])),
+                                      XML_FileName, "Attribute"], index=df.columns), ignore_index=True)
+            # Append all MCDS attributes for child, xPath with indexing removed, and XML file to list
+
+df.to_excel('MCDS_DCL_All_Entities.xlsx')
